8bec2bb34a94358e7731f57f41ca730de1997bc9
Jorge Bau Gracia <jbaugracia@gmail.com>
2019-09-06 10:18:13 +0200

    Initial commit

---
+++ src/main/kotlin/arithmetic/ArithmeticApplication.kt
@@ -0,0 +1,11 @@
+package arithmetic
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class ArithmeticApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ArithmeticApplication::class.java, *args)
+}
